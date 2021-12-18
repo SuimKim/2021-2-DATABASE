fe1edef47226c5c686f8b4cdd4f5de5e74d18959
김수임|Suim Kim <suim0215@gmail.com>
2021-12-18 15:32:57 +0900

    프로젝트 데이터 추가 코드

---
+++ 프로젝트데이터.sql
@@ -0,0 +1,114 @@
+insert into hotel values
+(1521, '글래드여의도', 2015, '서울영등포구', '02-1234-2141', '9호선 국회의사당역 4번 출구에서 도보 1분, 여의도 공원이 도보로 5분 거리입니다.');
+insert into hotel values
+(1412, '웨스틴조선서울', 2016, '서울중구', '02-125-7410', '신세계백화점 본점과 남대문 시장, 서울N타워, 청계천, 명동 등과 인접합니다.');
+insert into hotel values
+(1637, '시그니엘서울', 2018, '서울송파구', '02-7898-7858', '롯데호텔앤리조트의 최상급 호텔 브랜드로, 롯데월드타워 76층에서 101층에 위치해 있습니다.');
+insert into hotel values
+(2587, '제주신화월드', 2014, '제주서귀포시', '1670-8800', '제주신화월드는 5성급 부대시설과 서비스를 제공합니다.');
+insert into hotel values
+(2325, '제주신라호텔', 2017, '제주서귀포시', '064-735-5114', '제주 중문 관광 단지에 위치하고 있는 제주신라호텔은 사계절 휴양 리조트호텔입니다.');
+insert into hotel values
+(2147, '그랜드조선제주', 2021, '제주서귀포시', '164-738-6600', '서귀포에 위치한 그랜드 조선 제주는 2021년 1월 개관한 럭셔리 리조트형 호텔입니다.');
+insert into hotel values
+(3548, '파라다이스호텔부산', 2012, '부산해운대구', '051-742-2121', '해운대 최대 규모를 자랑하며 해운대 바다가 보이는 특 1급 호텔입니다.');
+insert into hotel values
+(3195, '파크하얏트부산', 2014, '부산해운대구', '051-990-1234', '멋진 바다와 광안대교 전경을 자랑하는 마린시티에 위치한 최고급 럭셔리 호텔입니다.');
+insert into hotel values
+(4963, '스카이베이호텔경포', 2019, '강원강릉시', '0507-1441-8903', '한국의 관동팔경의 하나로 최고의 휴양지인 강릉 경포대 앞에 위치해 있습니다.');
+insert into hotel values
+(4254, '라마다속초호텔', 2015, '강원속초시', '1577-0130', '대포항 바로 앞에 위치한 라마다 속초 호텔에서는 동해의 일출과 일몰을 감상 가능합니다.');
+
+insert into hoteluser values
+('diwnck154', '최승철', '남자', '1995-08-08', '010-1234-1234');
+insert into hoteluser values
+('angelS2', '윤정한', '남자', '1995-10-04', '010-0548-1209');
+insert into hoteluser values
+('yeahsu22', '홍지수', '여자', '1995-12-30', '010-2525-4545');
+insert into hoteluser values
+('budam6', '문준휘', '여자', '1996-06-05', '010-2264-6215');
+insert into hoteluser values
+('horanghae0', '권순영', '여자', '1996-06-15', '010-1010-4254');
+insert into hoteluser values
+('everywon17', '전원우', '남자', '1996-07-17', '010-4189-2196');
+insert into hoteluser values
+('univerS2', '이지훈', '남자', '1996-11-22', '010-5144-7897');
+insert into hoteluser values
+('imthe8', '서명호', '남자', '1997-11-07', '010-8588-2588');
+insert into hoteluser values
+('puppy46', '김민규', '남자', '1997-04-06', '010-8935-4236');
+insert into hoteluser values
+('galbizzim7', '이석민', '남자', '1997-02-18', '010-8951-8523');
+insert into hoteluser values
+('bburang23', '부승관', '남자', '1998-01-16', '010-2121-6556');
+insert into hoteluser values
+('chewsol5', '최한솔', '여자', '1998-02-18', '010-7897-4565');
+insert into hoteluser values
+('center78', '이차영', '여자', '1999-02-11', '010-1254-8956');
+
+insert into room values
+(1025, 'yes', '더블', '시티뷰', 1, 'no', 50000, 601, 1521);
+insert into room values
+(1026, 'yes', '퀸', '시티뷰', 2, 'yes', 100000, 802, 1521);
+insert into room values
+(1027, 'no', '퀸, 싱글', '마운틴뷰', 4, 'yes', 200000, 1024, 1521);
+insert into room values
+(1014, 'no', '더블, 싱글', '시티뷰', 2, 'no', 150000, 504, 1412);
+insert into room values
+(1015, 'yes', '싱글', '마운틴뷰', 1, 'yes', 50000, 702, 1412);
+insert into room values
+(1031, 'no', '더블', '시티뷰', 1, 'yes', 65000, 1814, 1637);
+insert into room values
+(1032, 'yes', '킹, 더블', '시티뷰', 4, 'no', 300000, 1902, 1637);
+insert into room values
+(2081, 'yes', '더블, 싱글', '오션뷰', 2, 'no', 150000, 703, 2587);
+insert into room values
+(2082, 'no', '퀸', '오션뷰', 2, 'yes', 150000, 907, 2587);
+insert into room values
+(2026, 'no', '더블', '시티뷰', 1, 'no', 55000, 503, 2325);
+insert into room values
+(2027, 'yes', '퀸, 더블', '오션뷰', 4, 'no', 250000, 709, 2325);
+insert into room values
+(2028, 'no', '킹, 퀸', '오션뷰', 5, 'no', 300000, 1012, 2325);
+insert into room values
+(2041, 'no', '싱글', '시티뷰', 1, 'yes', 50000, 802, 2147);
+insert into room values
+(3042, 'yes', '더블, 싱글', '오션뷰', 3, 'no', 150000, 1005, 3548);
+insert into room values
+(3043, 'no', '퀸, 더블', '오션뷰', 3, 'no', 200000, 1015, 3548);
+insert into room values
+(3096, 'yes', '더블', '시티뷰', 1, 'yeo', 55000, 508, 3195);
+insert into room values
+(3097, 'yes', '더블, 싱글', '오션뷰', 2, 'no', 200000, 908, 3195);
+insert into room values
+(3098, 'no', '킹', '오션뷰', 3, 'yes', 250000, 2012, 3195);
+insert into room values
+(4061, 'yes', '더블', '시티뷰', 1, 'no', 50000, 402, 4963);
+insert into room values
+(4053, 'yes', '킹, 퀸', '오션뷰', 5, 'yes', 350000, 1524, 4254);
+insert into room values
+(4054, 'no', '퀸', '오션뷰', 2, 'yes', 150000, 1526, 4254);
+
+insert into booking values
+(1012, '2021-11-27', '2021-11-29', '카드', 1, 'no', 1025, 'diwnck154');
+insert into booking values
+(1022, '2021-12-01', '2021-12-02', '카드', 2, 'yes', 1014, 'angelS2');
+insert into booking values
+(1013, '2021-12-04', '2021-12-05', '현금', 3, 'yes', 1032, 'yeahsu22');
+insert into booking values
+(1014, '2021-12-07', '2021-12-09', '포인트', 2, 'no', 2081, 'budam6');
+insert into booking values
+(1015, '2021-12-11', '2021-12-02', '카드', 1, 'no', 2026, 'horanghae0');
+insert into booking values
+(1016, '2021-12-12', '2021-12-15', '카드', 4, 'no', 2027, 'everywon17');
+insert into booking values
+(1017, '2021-12-14', '2021-12-15', '현금', 3, 'yes', 2028, 'univerS2');
+insert into booking values
+(1018, '2021-12-17', '2021-12-18', '카드', 3, 'yes', 3042, 'puppy46');
+insert into booking values
+(1019, '2021-12-25', '2021-12-26', '카드', 2, 'no', 3043, 'galbizzim7');
+insert into booking values
+(1020, '2021-12-24', '2021-12-26', '현금', 2, 'no', 3097, 'bburang23');
+insert into booking values
+(1021, '2021-12-31', '2022-01-01', '카드', 1, 'yes', 4061, 'center78');
+
